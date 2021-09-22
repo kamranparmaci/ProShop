88d22a4427cfd174e761322e217e266b182b5b63
kamrancode <kamranparmasii@gmail.com>
2021-09-22 23:19:35 +0330

    created userRoutes and userController and tested in postman. added registration and authorization methods

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const usrSchema = mongoose.Schema(
+const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
@@ -26,6 +27,19 @@ const usrSchema = mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", usrSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
